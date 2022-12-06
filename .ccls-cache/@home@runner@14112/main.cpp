e0e46fecead9d605f5f2cdfffd93fb23de638021
Illia Cherednik <91588797+Sakram4ik@users.noreply.github.com>
2022-12-06 18:24:15 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@14112/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctime>//для бібліотеки time
+#include <cstdlib>//для бібліотеки random
+using namespace std;
+int main(){
+	int arr[6][4];
+	int even = 0;//змінна для парних чисел рядка
+	srand(time(NULL));//отримує у вигляді параметра поточний системний час, який при кожному запуску програми буде різним. 
+	for (int i = 0; i < 6; ++i) { // для рядків 
+		for (int j = 0; j < 4; ++j) {// для стовпчиків
+			arr[i][j] =rand() %10;
+		}
+	}
+	for (int i = 0; i < 4; ++i) {
+		even = 0;//оновлює щоразу після циклу в рядку
+		for (int j = 0; j < 6; ++j) {
+			if (arr[j][i] % 2 == 0)//рахує кількість парних в стовпчику
+				even++;
+		}
+		cout << "for " << i << " column even number = " << even << endl;
+	}
+	for (int i = 0; i < 6; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			cout << arr[i][j] << ' ';
+		}//виведення двовимірного масиву
+		cout << endl;
+	}
+}
